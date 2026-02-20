9218a133ea4eaa00ebebce27a3e212f2d82c7ab1
dicarlophotoart-blip <dicarlophotoart@gmail.com>
2026-02-19 22:34:58 -0400

    Update fichas.js

--- fichas.js
+++ fichas.js
@@ -1,5 +1,24 @@
-<script>
+// Archivo temporal para poder guardar
 function generarMapa() {
-    // código del mapa con 3 puntos
+    const mapDiv = document.getElementById('chinaMap');
+    if (!mapDiv) return;
+    
+    const chart = echarts.init(mapDiv);
+    
+    chart.setOption({
+        series: [{
+            type: 'map',
+            map: 'china',
+            data: [
+                { name: '四川省', value: 4 },
+                { name: '辽宁省', value: 2 },
+                { name: '北京市', value: 1 }
+            ]
+        }]
+    });
 }
-</script>
+
+function actualizarPagina() {
+    // Temporalmente vacía
+    console.log('Página actualizada');
+}
